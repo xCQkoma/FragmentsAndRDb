f91deb6a6128ee9b1edc72aa042bc72ad8907606
xCQkoma <aa4sui2ren5@gmail.com>
2020-07-31 18:03:22 +0900

    relate room db

---
+++ app/src/main/java/com/example/android/fragmentsandrdb/WordRoomDatabase.java
@@ -0,0 +1,33 @@
+package com.example.android.fragmentsandrdb;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+    private static WordRoomDatabase INSTANCE;
+
+    static WordRoomDatabase getDatabase(final Context context){
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            .fallbackToDestructiveMigration()
+
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
